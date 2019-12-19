0bb64eff284f7592e506c71ba1c045fcd69537b1
Drew Blincoe <drewblincoe@yahoo.com>
2019-12-19 13:00:24 -0600

    added add item and remove item to checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,3 +10,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItems, itemToRemove) => {
+    return cartItems.filter(item => item.id !== itemToRemove.id);
+};
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+    const existing = cartItems.find(item => item.id === itemToRemove.id);
+
+    if (existing.quantity === 1) {
+        return cartItems.filter(item => item.id !== itemToRemove.id);
+    }
+
+    return cartItems.map(item =>
+        item.id === itemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+    );
+};
